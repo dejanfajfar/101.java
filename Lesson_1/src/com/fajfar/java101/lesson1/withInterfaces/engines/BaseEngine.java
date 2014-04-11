d3b3a929bd8b519dc6fccdaffe1f967de51ad31e
Dejan Fajfar <dejan@fajfar.com>
2014-04-11 13:41:02 +0200

    Added base engine

--- Lesson_1/src/com/fajfar/java101/lesson1/withInterfaces/engines/BaseEngine.java
+++ Lesson_1/src/com/fajfar/java101/lesson1/withInterfaces/engines/BaseEngine.java
@@ -12,35 +12,63 @@ public abstract class BaseEngine implements Engine {
     private int minRpm;
     private int maxRpm;
 
+    protected void setMinRpm(int minRpm){
+        this.minRpm = minRpm;
+    }
+
+    protected void setMaxRpm(int maxRpm){
+        this.maxRpm = maxRpm;
+    }
+
     @Override
     public void turnOn() {
-
+        if(!isRunning()){
+            doTurnOn();
+        }
     }
+
+    protected abstract void doTurnOn();
 
     @Override
     public void turnOff() {
-
+        if(isRunning()){
+            doTurnOff();
+        }
     }
+
+    protected abstract void doTurnOff();
 
     @Override
     public int getRpm() {
+        if(isRunning()){
+            return doGetRpm();
+        }
         return 0;
     }
 
+    protected abstract int doGetRpm();
+
     @Override
     public int idle() {
-        return 0;
+        if(isRunning()){
+            setRpm(this.minRpm);
+        }
+        return getRpm();
     }
 
     @Override
     public boolean isRunning() {
-        return false;
+        return doCheckIsRunning();
     }
+
+    protected abstract boolean doCheckIsRunning();
 
     @Override
     public boolean canHandleRpm(int rpm) {
-        return false;
+        return doCanHandleRpm(rpm);
     }
+
+    protected abstract boolean doCanHandleRpm(int rpm);
 
     @Override
     public int goFlatOut() {
